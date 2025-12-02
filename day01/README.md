3be1235e8e6f5250fb31f7d9b022f8858b59325e
Felix <viola@cs.uni-bonn.de>
2025-12-02 15:18:45 +0100

    more testing, but still does not work

--- day01/README.md
+++ day01/README.md
@@ -1,3 +1,5 @@
-# Day 01
+# (Day 01)[https://adventofcode.com/2025/day/1]
 
 btw, `0x434C49434B` is ASCII for `CLICK`
+
+6475
--- day01/src/main.rs
+++ day01/src/main.rs
@@ -17,46 +17,58 @@ fn main() {
     let mut cnt = 0;
 
     // File hosts.txt must exist in the current path
-    if let Ok(lines) = read_lines("./input.txt") {
+    if let Ok(lines) = read_lines("./melina-input.txt") {
         // Consumes the iterator, returns an (Optional) String
         println!("     ->   50 /   50");
         for mut line in lines.map_while(Result::ok) {
-            let direction = line.chars().next().unwrap();
-            let mut distance = line.split_off(1).parse::<i32>().unwrap();
-            if direction == 'L' {
-                distance = distance * -1;
-            }
+            let direction = if line.chars().next().unwrap() == 'R' {
+                1
+            } else {
+                -1
+            };
+
+            let distance = line.split_off(1).parse::<i32>().unwrap() * direction;
+
             let new_pos = pos + distance;
+            let spin_overs = ((distance * direction) as f32 / 100_f32).floor() as i32;
+            let mut change = 0;
 
-            let mut zero_change = false;
-            let mut zero = false;
-
-            if new_pos == 0 || new_pos == 100 {
-                zero = true;
-            } else if pos > 0 && new_pos < 0 {
-                zero_change = true;
-            } else if new_pos > 100 {
-                zero_change = true;
+            if new_pos % 100 == 0 {
+                // landet genau auf 0
+                if spin_overs == 0 {
+                    // ohne "überdrehen"
+                    change = 1;
+                } else {
+                    // mit überdrehen, also auf 0 landen + spinovers
+                    change = 1 + spin_overs;
+                }
+            } else if direction == -1 && new_pos < 0 {
+                // nach links drehen und über 0 gegangen
+                if spin_overs > 0 {
+                    change = 1 + spin_overs;
+                } else {
+                    change = 1;
+                }
+            } else if direction == 1 && new_pos > 100 {
+                // nach rechts drehen und über 0 gegangen
+                if spin_overs > 0 {
+                    change = spin_overs;
+                } else {
+                    change = 1;
+                }
+            } else if pos == 0 && spin_overs > 0 {
+                change = spin_overs;
             }
 
-            if zero || zero_change {
-                cnt += 1;
-            }
+            cnt += change;
 
             pos = (new_pos + 100) % 100;
             print!("{:>4} -> {:>4} / {:>4}   ", distance, new_pos, pos);
-            if zero {
-                print!("*0*");
-            }
 
-            if zero_change {
-                print!("change")
-            }
-
-            println!();
-            if cnt > 10 {
-                break;
-            }
+            println!("{cnt:>4} ({})", change);
+            // if cnt > 10 {
+            //     break;
+            // }
         }
     }
 
